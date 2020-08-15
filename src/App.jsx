27c3ebcf8a2954c8a84deaaa04fbeb5409c05df5
drigols <drigols.creative@gmail.com>
2020-08-14 21:42:09 -0300

    :arrow_up: Add create project structure and add SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './vendors/font-awesome/css/font-awesome.min.css';
+import './vendors/freelancer/freelancer.min.css';
+import './css/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <React.Fragment>
+        <h1>Hello World</h1>
+      </React.Fragment>
     </div>
   );
 }

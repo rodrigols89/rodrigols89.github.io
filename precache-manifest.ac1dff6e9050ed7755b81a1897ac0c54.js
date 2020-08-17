56566755c71f30b12fe6d4a880dbb4d063fd4515
drigols <drigols.creative@gmail.com>
2020-08-16 23:46:42 -0300

    Updates

---
+++ precache-manifest.ac1dff6e9050ed7755b81a1897ac0c54.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da37c5f3608d7d1ca35435795a15e44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36c1337f5b8cdfe92bdd",
+    "url": "/static/css/main.f47491bb.chunk.css"
+  },
+  {
+    "revision": "2b9dcdece30a3c3acecc",
+    "url": "/static/js/2.76bc1db7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.76bc1db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c1337f5b8cdfe92bdd",
+    "url": "/static/js/main.63455d7b.chunk.js"
+  },
+  {
+    "revision": "ed9401a9ddc1b8774286",
+    "url": "/static/js/runtime-main.e208e0ab.js"
+  },
+  {
+    "revision": "962ec14def36cfbc63344802e0b86482",
+    "url": "/static/media/background-white.962ec14d.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "271bc2e22cd45b91bfa770d0a8e8d53a",
+    "url": "/static/media/studies-logo.271bc2e2.gif"
+  }
+]);

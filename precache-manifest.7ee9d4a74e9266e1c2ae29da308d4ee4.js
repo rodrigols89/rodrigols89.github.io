2aa7fb75534312b5c2464a884f1a8d80618c26b0
drigols <drigols.creative@gmail.com>
2020-08-17 00:14:11 -0300

    Updates

---
+++ precache-manifest.7ee9d4a74e9266e1c2ae29da308d4ee4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09721790ad5f2170196e32fc5c9ef079",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "506344548c375aaee9b4",
+    "url": "/home/static/css/main.fe2da071.chunk.css"
+  },
+  {
+    "revision": "2b9dcdece30a3c3acecc",
+    "url": "/home/static/js/2.76bc1db7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/home/static/js/2.76bc1db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506344548c375aaee9b4",
+    "url": "/home/static/js/main.63455d7b.chunk.js"
+  },
+  {
+    "revision": "63631d24cecf2c696290",
+    "url": "/home/static/js/runtime-main.d567b3e2.js"
+  },
+  {
+    "revision": "962ec14def36cfbc63344802e0b86482",
+    "url": "/home/static/media/background-white.962ec14d.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/home/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/home/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/home/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/home/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/home/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "271bc2e22cd45b91bfa770d0a8e8d53a",
+    "url": "/home/static/media/studies-logo.271bc2e2.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac1dff6e9050ed7755b81a1897ac0c54.js"
+  "/home/precache-manifest.7ee9d4a74e9266e1c2ae29da308d4ee4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

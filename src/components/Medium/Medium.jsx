aecdc257cfcee7831516d9f5eafc0cc157d56f9f
drigols <drigols.creative@gmail.com>
2020-09-18 18:47:44 -0300

    :rocket: upgrade Medium and studies project

--- src/components/Medium/Medium.jsx
+++ src/components/Medium/Medium.jsx
@@ -32,45 +32,91 @@ const Medium = () => {
             </div>
           </div>
 
-          <div className="card-deck">
 
-            <div className="card">
-              <img className="card-img-top" src={mediumData[0]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
-              <div className="card-body">
-                <a href={mediumData[0]?.link}>
-                  <h5 className="card-title">{mediumData[0]?.title || "Soon..."}</h5>
-                </a>
+          <div className="row row-padding">
+            <div className="card-deck">
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[0]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[0]?.link}>
+                    <h5 className="card-title">{mediumData[0]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[0]?.pubDate}</small>
+                </div>
               </div>
-              <div className="card-footer">
-                <small className="text-muted">Date: {mediumData[0]?.pubDate}</small>
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[1]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[1]?.link}>
+                    <h5 className="card-title">{mediumData[1]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[1]?.pubDate}</small>
+                </div>
               </div>
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[2]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[2]?.link}>
+                    <h5 className="card-title">{mediumData[2]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[2]?.pubDate}</small>
+                </div>
+              </div>
+
             </div>
-
-            <div className="card">
-              <img className="card-img-top" src={mediumData[1]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
-              <div className="card-body">
-                <a href={mediumData[1]?.link}>
-                  <h5 className="card-title">{mediumData[1]?.title || "Soon..."}</h5>
-                </a>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Date: {mediumData[1]?.pubDate}</small>
-              </div>
-            </div>
-
-            <div className="card">
-              <img className="card-img-top" src={mediumData[2]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
-              <div className="card-body">
-                <a href={mediumData[2]?.link}>
-                  <h5 className="card-title">{mediumData[2]?.title || "Soon..."}</h5>
-                </a>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Date: {mediumData[2]?.pubDate}</small>
-              </div>
-            </div>
-
           </div>
+
+
+          <div className="row">
+            <div className="card-deck">
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[3]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[3]?.link}>
+                    <h5 className="card-title">{mediumData[3]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[3]?.pubDate}</small>
+                </div>
+              </div>
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[4]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[4]?.link}>
+                    <h5 className="card-title">{mediumData[4]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[4]?.pubDate}</small>
+                </div>
+              </div>
+
+              <div className="card">
+                <img className="card-img-top" src={mediumData[5]?.thumbnail || "https://image.freepik.com/free-photo/coming-soon-neon-sign-purple-blue-glow-neon-text-brick-wall-lit-by-neon-lamps-night-lighting-wall_124343-10.jpg"} alt="img"/>
+                <div className="card-body">
+                  <a href={mediumData[5]?.link}>
+                    <h5 className="card-title">{mediumData[5]?.title || "Soon..."}</h5>
+                  </a>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Date: {mediumData[5]?.pubDate}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
 
         </div>
       </section>

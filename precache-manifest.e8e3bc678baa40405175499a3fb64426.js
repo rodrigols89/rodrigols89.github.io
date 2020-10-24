b1e1447de1401dde36f2a83cd0e35e2b01f38733
drigols <drigols.creative@gmail.com>
2020-10-24 19:45:44 -0300

    Updates

---
+++ precache-manifest.e8e3bc678baa40405175499a3fb64426.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e930c9920bfc733d8b118393d938bd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e741f31b7d06b67474",
+    "url": "/static/css/main.5a633152.chunk.css"
+  },
+  {
+    "revision": "d0092879aa872fdd7d15",
+    "url": "/static/js/2.58967acf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58967acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e741f31b7d06b67474",
+    "url": "/static/js/main.9030c403.chunk.js"
+  },
+  {
+    "revision": "33445b0c149a7d6e90b0",
+    "url": "/static/js/runtime-main.0570c957.js"
+  },
+  {
+    "revision": "962ec14def36cfbc63344802e0b86482",
+    "url": "/static/media/background-white.962ec14d.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "6d1cbf2999655901ed01952135a88b89",
+    "url": "/static/media/studies-project-logo.6d1cbf29.gif"
+  },
+  {
+    "revision": "dcf2dae4d65d19a314d87705e2ac91cc",
+    "url": "/static/media/studies-project-logo.dcf2dae4.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf976b97ceb18abe55bdca126bbe3d41.js"
+  "/precache-manifest.e8e3bc678baa40405175499a3fb64426.js"
 );
 
 self.addEventListener('message', (event) => {

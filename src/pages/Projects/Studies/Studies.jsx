330c58acc9244655d1e3073916b8d5a05e953027
drigols <drigols.creative@gmail.com>
2021-08-15 15:17:55 -0300

    :rocket: ENG to PT

--- src/pages/Projects/Studies/Studies.jsx
+++ src/pages/Projects/Studies/Studies.jsx
@@ -27,7 +27,7 @@ const Studies = () => {
               <ul>
                 <li><strong>Topics:</strong></li>
                 <ul>
-                  <li>Programming</li>
+                  <li>Programming Languages</li>
                   <li>Algorithms & Data Structures</li>
                   <li>Database</li>
                   <li>Computer Science Fundamentals</li>
@@ -36,9 +36,9 @@ const Studies = () => {
                   <li>Others...</li>
                 </ul>
               </ul>
-              <a href="https://github.com/drigols/studies" rel="noopener"><span className="section-title"> → [Project in GitHub]</span></a><br/><br/>
+              <a href="https://github.com/drigols/estudos" rel="noopener"><span className="section-title"> → [Project in GitHub]</span></a><br/><br/>
               <strong>NOTE:</strong><br/>
-              Many of my studies are in Brazilian Portuguese... <a href="https://github.com/drigols/studies/tree/master-pt-br"><strong>You can see here</strong></a>.<br/><br/>
+              Many of my studies are in Brazilian Portuguese.<br/><br/>
             </div>
             <ul className="list-inline item-details">
               <li>

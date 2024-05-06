a49b892fc66cca38331cf56c7dd0f1dde47cb0e4
drigols <drigols.creative@gmail.com>
2024-05-06 06:54:00 -0300

    Update About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
 
                 <div className="row">
                     <div className="text-cover">
-                        <h1 className="name">Ro<span className="drigols">drigo</span>  <span className="drigols">L</span>eite  da  <span className="drigols">S</span>ilva - <span className="drigols">drigols</span></h1>
+                        <h1 className="name"><span className="drigols">Rodrigo</span>  <span className="drigols">L</span>eite  da  <span className="drigols">S</span>ilva</h1>
                     </div>
                     <div className="col-lg-12">
                         <p>
@@ -26,7 +26,7 @@ const About = () => {
                             <ul>
                                 <li>I am a Software Developer with a degree in <strong>Analysis and Systems Development</strong> from <a href="https://www.uninassau.edu.br/" target="_blank" rel="noopener noreferrer">Mauricio of Nassau</a> College</li>
                                 <li>Currently I am Graduating (Specialization) in <strong>Applied Statistics</strong> at <a href="https://faculdadefocus.com.br/" target="_blank" rel="noopener noreferrer">Focus</a> College.</li>
-                                <li>I have experience in Personal Projects involving C++, Python, Data Science, Machine Learning, and Open-Source.</li>
+                                <li>I have experience in Personal Projects involving Python, Data Science, and Open-Source.</li>
                             </ul>
                         </p>
                     </div>

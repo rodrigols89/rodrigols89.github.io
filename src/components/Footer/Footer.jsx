22e71016dcb3a9e9a50a9c8be90b9977a6f19bab
drigols <drigols.creative@gmail.com>
2024-05-06 07:41:45 -0300

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
             <hr className="style-four" />
             <address>
               Brazil, State of Paraíba, Zip code: 58398-000<br />
-              Cell phone number: 55 83 996500112 (WhatsApp)<br />
-              Cell phone number: 55 83 996241663 (Call)<br />
-              <strong>E-mail: </strong><a href="mailto:#">drigols.creative@gmail.com</a>
+              Phone number: 55 83 996500112 (WhatsApp)<br />
+              Phone number: 55 83 996241663 (Call)<br />
+              <strong>E-mail: </strong><a href="mailto:rodrigols89.inbox@gmail.com">rodrigols89.inbox@gmail.com</a>
             </address>
           </div>
 
@@ -24,10 +24,10 @@ const Footer = () => {
             <hr className="style-four" />
             <ul className="list-inline">
               <li className="list-inline-item">
-                <a href="https://www.linkedin.com/in/drigols" className="btn-social btn-outline" rel="noopener" title="Linkedin"><span className="sr-only">Linkedin</span><i className="fa fa-fw fa-linkedin"></i></a>
+                <a href="https://www.linkedin.com/in/rodrigols89" className="btn-social btn-outline" title="Linkedin" target="_blank" rel="noopener noreferrer"><span className="sr-only">Linkedin</span><i className="fa fa-fw fa-linkedin"></i></a>
               </li>
               <li className="list-inline-item">
-                <a href="https://github.com/drigols" className="btn-social btn-outline" rel="noopener" title="GitHub"><span className="sr-only">GitHub</span><i className="fa fa-fw fa-github"></i></a>
+                <a href="https://github.com/rodrigols89" className="btn-social btn-outline" title="GitHub" target="_blank" rel="noopener noreferrer"><span className="sr-only">GitHub</span><i className="fa fa-fw fa-github"></i></a>
               </li>
             </ul>
           </div>

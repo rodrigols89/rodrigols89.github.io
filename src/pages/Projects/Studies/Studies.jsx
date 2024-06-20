8eb59d07f7b6195892553aaffa4c89233d80d46e
rodrigols89 <rodrigols89.inbox@gmail.com>
2024-06-20 09:22:57 -0300

    Update Studies project to Portuguese

--- src/pages/Projects/Studies/Studies.jsx
+++ src/pages/Projects/Studies/Studies.jsx
@@ -8,62 +8,53 @@ import studiesLogo from './images/studies-logo-300x200.gif';
 import './Studies.scss';
 
 const Studies = () => {
-  return (
-    <div>
-      <SecondNavbar />
+    return (
+        <div>
+            <SecondNavbar />
 
-      <div className="container-fluid project">
-        <div className="row modal-body">
+            <div className="container-fluid project">
+                <div className="row modal-body">
 
-          <div className="col-sm-6">
-            <h2 className="text-center">Studies</h2>
-            <hr className="star-primary"/>
-            <img src={studiesLogo} className="img-fluid img-thumbnail center" alt="img"/>
-          </div>
+                    <div className="col-sm-6">
+                        <h2 className="text-center">Studies</h2>
+                        <hr className="star-primary" />
+                        <img src={studiesLogo} className="img-fluid img-thumbnail center" alt="img" />
+                    </div>
 
-          <div className="col-sm-6 text-project">
-            <div>
-              My <i>theoretical</i> and <i>practical</i> studies:<br/>
-              <ul>
-                <li><strong>Topics:</strong></li>
-                <ul>
-                  <li>Programming Languages</li>
-                  <li>Algorithms and Data Structures</li>
-                  <li>Database</li>
-                  <li>Computer Science Fundamentals</li>
-                  <li>Artificial Intelligence</li>
-                  <li>Mathematics</li>
-                  <li>Others...</li>
-                </ul>
-              </ul>
-              <a href="https://github.com/rodrigols89/studies" target="_blank" rel="noopener noreferrer"><span className="section-title"> → [Project in GitHub]</span></a>
-              <br/>
-              <br/>
-              <strong>NOTE:</strong><br/>
-              Many of my studies are in Portuguese (Brazil).
-              <br/>
-              <br/>
+                    <div className="col-sm-6 text-project">
+                        <div>
+                            O projeto <strong>Studies</strong> é escrito como tutoriais para que outros possam aprender a partir dos meus estudos. Alguns tópicos incluem:
+                            <ul>
+                                <li>Linguagens de Programação.</li>
+                                <li>Banco de Dados.</li>
+                                <li>Matemática</li>
+                                <li>Inteligência Artificial.</li>
+                                <li>Algoritmos e Estruturas de Dados.</li>
+                            </ul>
+                            <a href="https://github.com/rodrigols89/studies" target="_blank" rel="noopener noreferrer"><span className="section-title"> → [Project on GitHub]</span></a>
+                            <br />
+                            <br />
+                        </div>
+                        <ul className="list-inline item-details">
+                            <li>
+                                Client: <strong>Myself</strong>
+                            </li>
+                            <li>
+                                Date: <strong>September 2021 to the present day</strong>
+                            </li>
+                            <li>
+                                Service: <strong>Studies</strong>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
             </div>
-            <ul className="list-inline item-details">
-              <li>
-                Client: <strong>Myself</strong>
-              </li>
-              <li>
-                Date: <strong>September 2021 to the present day</strong>
-              </li>
-              <li>
-                Service: <strong>Studies</strong>
-              </li>
-            </ul>
-          </div>
+
+            <Footer />
 
         </div>
-      </div>
-
-      <Footer />
-
-    </div>
-  );
+    );
 }
 
 export default Studies;

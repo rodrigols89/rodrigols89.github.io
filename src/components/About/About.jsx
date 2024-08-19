0d806e7307b24adbc2b9236dc27544ecc9a82db5
rodrigols89 <rodrigols89.inbox@gmail.com>
2024-08-19 14:13:20 -0300

    Updatre about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,7 +25,17 @@ const About = () => {
                         <p>
                             <ul>
                                 <li>Sou Desenvolvedor de Software com experiência em Projetos Pessoais e de Código Aberto envolvendo Python e Ciência de Dados.</li>
-                                <li>Com formação em <strong>Análise e Desenvolvimento de Sistemas</strong>, atualmente estou especializando em <strong>Ciência de Dados</strong> e <strong>Segurança da Informação</strong> com o objetivo de desenvolver software inteligente para cibersegurança.</li>
+                                <li>Com formação em <strong>Análise e Desenvolvimento de Sistemas</strong>, atualmente estou especializando em <strong>Ciência de Dados</strong> com o objetivo de aprender a gerar valor nos projetos em que trabalho a partir de dados.</li>
+                            </ul>
+                        </p>
+                        <p>
+                            <strong>Tecnologias:</strong>
+                            <ul>
+                                <li><strong>Linguagem de programação principal:</strong> Python.</li>
+                                <li><strong>Linguagens de programação secundárias:</strong> C, C++, Java, JavaScript.</li>
+                                <li><strong>Banco de Dados:</strong> SQL, PostgreSQL, MySQL.</li>
+                                <li><strong>Desenvolvimento Web:</strong> Django, React, HTML, CSS.</li>
+                                <li><strong>Ferramentas:</strong> Git, GitHub (GitHub Actions CI), Docker.</li>
                             </ul>
                         </p>
                     </div>

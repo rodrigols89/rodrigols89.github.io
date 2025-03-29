314ca1fdf61c3a1d272d42bf80b1ff26c9069523
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-03-29 19:20:01 -0300

    Update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,27 +15,23 @@ const About = () => {
                 </div>
 
                 <div className="row">
-                    <div className="text-cover">
-                        <h1 className="name"><span className="drigols">Rodrigo</span>  <span className="drigols">L</span>eite  da  <span className="drigols">S</span>ilva</h1>
-                    </div>
                     <div className="col-lg-12">
                         <p>
                             Olá a todos!
                         </p>
                         <p>
                             <ul>
-                                <li>Sou Desenvolvedor de Software com experiência em Projetos Pessoais e de Código Aberto envolvendo Python e Ciência de Dados.</li>
-                                <li>Com formação em <strong>Análise e Desenvolvimento de Sistemas</strong>, atualmente estou me especializando em <strong>Engenharia de Software</strong> com o objetivo de aprimorar minhas habilidades em engenharia, gerenciamento e qualidade de software.</li>
+                                <li style={{ marginBottom: "10px" }}>Me chamo <strong>Rodrigo Leite da Silva</strong>, sou desenvolvedor de software e graduado em <strong>Análise e Desenvolvimento de Sistemas</strong>, com experiência em projetos pessoais e open-source, especialmente com <i>Python</i>.</li>
+                                <li>Atualmente, estou cursando <strong>Matemática</strong>, pois acredito que a integração entre os fundamentos computacionais e uma visão analítica aprofundada da matemática é essencial para resolver problemas atuais, principalmente no âmbito da <i>Inteligência Artificial</i>. Assim, busco unir rigor teórico e criatividade prática para desenvolver soluções inteligentes e eficientes.</li>
                             </ul>
                         </p>
                         <p>
                             <strong>Tecnologias:</strong>
                             <ul>
-                                <li><strong>Linguagem de programação principal:</strong> Python.</li>
-                                <li><strong>Linguagens de programação secundárias:</strong> C, C++, Java, JavaScript.</li>
-                                <li><strong>Banco de Dados:</strong> SQL, PostgreSQL, MySQL.</li>
-                                <li><strong>Desenvolvimento Web:</strong> Django, React, HTML, CSS.</li>
-                                <li><strong>Ferramentas:</strong> Git, GitHub (GitHub Actions CI), Docker.</li>
+                                <li><strong>Linguagens de Programação:</strong> Python, JavaScript, Java.</li>
+                                <li><strong>Banco de Dados:</strong> SQL, MySQL, PostgreSQL.</li>
+                                <li><strong>Desenvolvimento Web:</strong> HTML, CSS, Django, React.</li>
+                                <li><strong>Ferramentas:</strong> Git, GitHub (Actions), Docker.</li>
                             </ul>
                         </p>
                     </div>
